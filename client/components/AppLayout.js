743918c628cf26fc9c227727c54b70b5fe411370
Elly <ellyhahn@gmail.com>
2019-05-29 22:56:49 -0700

    Add _app.js and grid layout

--- client/components/AppLayout.js
+++ client/components/AppLayout.js
@@ -1,21 +1,64 @@
-import React from 'react';
-import Link from 'next/link';
-import { Menu, Input, Button } from 'antd';
+import React from "react";
+import Link from "next/link";
+import PropTypes from "prop-types";
+import { Avatar, Button, Card, Menu, Input, Row, Col } from "antd";
+
+const dummy = {
+  userid: 'elly',
+  post: [],
+  followings: [],
+  followers: [],
+};
 
 const AppLayout = ({ children }) => {
   return (
     <div>
       <Menu mode="horizontal">
-        <Menu.Item key="home"><Link href="/"><a>nodebird</a></Link></Menu.Item>
-        <Menu.Item key="profile"><Link href="/profile"><a>profile</a></Link></Menu.Item>
+        <Menu.Item key="home">
+          <Link href="/">
+            <a>nodebird</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="profile">
+          <Link href="/profile">
+            <a>profile</a>
+          </Link>
+        </Menu.Item>
         <Menu.Item key="search">
-          <Input.Search enterButton style={{ verticalAlign: 'middle' }} />
+          <Input.Search enterButton style={{ verticalAlign: "middle" }} />
         </Menu.Item>
       </Menu>
-      <Link href="/signup"><a><Button>Sign up</Button></a></Link>
-      {children}
+      <Link href="/signup">
+        <a>
+          <Button>Sign up</Button>
+        </a>
+      </Link>
+      <Row>
+        <Col xs={24} md={6}>
+          <Card
+            actions={[
+              <div key="twit">Twit<br />{dummy.post.length}</div>,
+              <div key="following">Followings<br />{dummy.followings.length}</div>,
+              <div key="follower">Followers<br />{dummy.followers.length}</div>,
+            ]}
+          >
+            <Card.Meta
+              avatar={<Avatar>{dummy.userid[0]}</Avatar>}
+              title={dummy.userid}
+            />
+          </Card>
+        </Col>
+        <Col xs={24} md={12}>
+          {children}           
+        </Col>
+        <Col xs={24} md={6}>third</Col>
+      </Row>
     </div>
   );
+};
+
+AppLayout.propTypes = {
+  children: PropTypes.elementType,
 };
 
 export default AppLayout;
--- client/pages/signup.js
+++ client/pages/signup.js
@@ -1,71 +1,89 @@
-import React, { useState } from 'react';
-import AppLayout from '../components/AppLayout';
-import Head from 'next/head';
-import { Button, Form, Input } from 'antd';
+import React, { useState, useCallback } from "react";
+import { Button, Form, Input } from "antd";
 
 const Signup = () => {
-  const [passwordCheck, setPasswordCheck] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState("");
   const [passwordError, setPasswordError] = useState(false);
 
   const useInput = (initValue = null) => {
     const [value, setter] = useState(initValue);
-    const handler = (e) => {
+    const handler = useCallback(e => {
       setter(e.target.value);
-    };
+    }, []);
     return [value, handler];
-  }
-
-  const [id, onChangeId] = useInput('');
-  const [password, onChangePassword] = useInput('');
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (password !== passwordCheck) {
-      return setPasswordError(true);
-    }
-    console.log({
-      id,
-      password,
-      passwordCheck,
-    })
   };
 
-  const onChangePasswordCheck = (e) => {
-    setPasswordError(e.target.value !== password);
-    setPasswordCheck(e.target.value);
-  };
+  const [id, onChangeId] = useInput("");
+  const [password, onChangePassword] = useInput("");
+
+  const onSubmit = useCallback(
+    e => {
+      e.preventDefault();
+      if (password !== passwordCheck) {
+        return setPasswordError(true);
+      }
+      console.log({
+        id,
+        password,
+        passwordCheck
+      });
+    },
+    [password, passwordCheck]
+  );
+
+  const onChangePasswordCheck = useCallback(
+    e => {
+      setPasswordError(e.target.value !== password);
+      setPasswordCheck(e.target.value);
+    },
+    [password]
+  );
 
   return (
-  <>
-    <Head>
-      <title>NodeBird</title>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.css" />
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.js" />
-    </Head>
-    <AppLayout>
-      <Form onSubmit = {onSubmit} style={{ padding: 10 }}>
+    <>
+      <Form onSubmit={onSubmit} style={{ padding: 10 }}>
         <div>
           <label htmlFor="user-id">id</label>
           <br />
-          <Input name="user-id" value={id} requred="true" onChange={onChangeId} />
+          <Input
+            name="user-id"
+            value={id}
+            requred="true"
+            onChange={onChangeId}
+          />
         </div>
         <div>
           <label htmlFor="user-password">password</label>
           <br />
-          <Input name="user-password" type="password" value={password} requred="true" onChange={onChangePassword} />
+          <Input
+            name="user-password"
+            type="password"
+            value={password}
+            requred="true"
+            onChange={onChangePassword}
+          />
         </div>
         <div>
           <label htmlFor="user-password-check">password check</label>
           <br />
-          <Input name="user-password-check" type="password" value={passwordCheck} requred="true" onChange={onChangePasswordCheck} />
-          {passwordError && <div style={{ color: 'red' }}>Password is not match.</div>}
+          <Input
+            name="user-password-check"
+            type="password"
+            value={passwordCheck}
+            requred="true"
+            onChange={onChangePasswordCheck}
+          />
+          {passwordError && (
+            <div style={{ color: "red" }}>Password is not match.</div>
+          )}
         </div>
         <div style={{ marginTop: 10 }}>
-          <Button type="primary" htmlType="submit">Sign Up</Button>
+          <Button type="primary" htmlType="submit">
+            Sign Up
+          </Button>
         </div>
       </Form>
-    </AppLayout>
-  </>
+    </>
   );
 };
 
