1b04ee9b408ddc81f46aaf53e54d1e6c76fc69de
Elly <ellyhahn@gmail.com>
2019-05-30 22:17:46 -0700

    Add profile page

--- client/pages/profile.js
+++ client/pages/profile.js
@@ -1,7 +1,53 @@
 import React from "react";
+import { Form, Input, Button, List, Card, Icon } from "antd";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div>
+      <Form
+        style={{
+          marginBottom: "20px",
+          border: "1px solid #d9d9d9",
+          padding: "20px"
+        }}
+      >
+        <Input addonBefore="Name" />
+        <Button type="primary">Edit</Button>
+      </Form>
+      <List
+        style={{ marginButton: "20px" }}
+        grid={{ gutter: 4, xs: 2, md: 3 }}
+        size="small"
+        header={<div>Followers</div>}
+        loadMore={<Button style={{ width: "100%" }}>more</Button>}
+        bordered
+        dataSource={["ab", "cde", "fgeds"]}
+        renderItem={item => (
+          <List.Item style={{ marginTop: "20px" }}>
+            <Card actions={[<Icon type="stop" />]}>
+              <Card.Meta description={item} />
+            </Card>
+          </List.Item>
+        )}
+      />
+      <List
+        style={{ marginButton: "20px" }}
+        grid={{ gutter: 4, xs: 2, md: 3 }}
+        size="small"
+        header={<div>Followings</div>}
+        loadMore={<Button style={{ width: "100%" }}>more</Button>}
+        bordered
+        dataSource={["ab", "cde", "fgeds"]}
+        renderItem={item => (
+          <List.Item style={{ marginTop: "20px" }}>
+            <Card actions={[<Icon type="stop" />]}>
+              <Card.Meta description={item} />
+            </Card>
+          </List.Item>
+        )}
+      />
+    </div>
+  );
 };
 
 export default Profile;
