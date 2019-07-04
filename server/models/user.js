b79276d7206bc357df78eb480c71eec5d5640a36
Elly <ellyhahn@gmail.com>
2019-07-04 16:26:28 -0700

    Add hashtag and comment orm

--- server/models/user.js
+++ server/models/user.js
@@ -19,8 +19,8 @@ module.exports = (sequelize, DataTypes) => {
   });
 
   User.associate = (db) => {
-    db.User.hasMany(db.post);
-    db.User.hasMany(db.comment);
+    db.User.hasMany(db.Post);
+    db.User.hasMany(db.Comment);
   };
 
   return User;
