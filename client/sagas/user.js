4ac464db0a3579b72b6f4642ef24e006b31d3adc
Elly <ellyhahn@gmail.com>
2019-07-21 17:43:42 -0700

    Undate user.js

--- client/sagas/user.js
+++ client/sagas/user.js
@@ -1,18 +1,62 @@
 import { all, call, fork, put, takeLatest, takeEvery } from 'redux-saga/effects';
-import { 
-  LOG_IN_REQUEST, 
-  LOG_IN_SUCCESS, 
-  LOG_IN_FAILURE, 
+import axios from "axios";
+import {
+  LOAD_USER_REQUEST,
+  LOAD_USER_SUCCESS, 
+  LOAD_USER_FAILURE,
   SIGN_UP_REQUEST, 
   SIGN_UP_SUCCESS, 
   SIGN_UP_FAILURE,
+  LOG_IN_REQUEST, 
+  LOG_IN_SUCCESS, 
+  LOG_IN_FAILURE, 
   LOG_OUT_REQUEST, 
   LOG_OUT_SUCCESS, 
-  LOG_OUT_FAILURE, 
+  LOG_OUT_FAILURE,
+  FOLLOW_USER_REQUEST,
+  FOLLOW_USER_SUCCESS,
+  FOLLOW_USER_FAILURE,
+  LOAD_FOLLOWERS_REQUEST,
+  LOAD_FOLLOWERS_SUCCESS,
+  LOAD_FOLLOWERS_FAILURE,
+  UNFOLLOW_USER_REQUEST,
+  UNFOLLOW_USER_SUCCESS,
+  UNFOLLOW_USER_FAILURE,
+  LOAD_FOLLOWINGS_REQUEST,
+  LOAD_FOLLOWINGS_SUCCESS,
+  LOAD_FOLLOWINGS_FAILURE,
+  REMOVE_FOLLOWER_SUCCESS,
+  REMOVE_FOLLOWER_FAILURE,
+  REMOVE_FOLLOWER_REQUEST,
+  EDIT_USERNAME_REQUEST,
+  EDIT_USERNAME_SUCCESS,
+  EDIT_USERNAME_FAILURE,
 } from '../reducers/user';
-import axios from "axios";
 
 axios.defaults.baseURL ='http://localhost:8080/api';
+
+
+function signUpAPI(signUpData) {
+  return axios.post('/user', signUpData);
+};
+
+function* signUp(action) {
+  try {
+    yield call(signUpAPI, action.data);
+    yield put({
+      type: SIGN_UP_SUCCESS,
+    });
+  } catch (e) {
+    console.error(e);
+    yield put({
+      type: SIGN_UP_FAILURE,
+      error: e,
+    });
+  }
+}
+function* watchSignUp() {
+  yield takeEvery(SIGN_UP_REQUEST, signUp)
+}
 
 function logInAPI(loginData) {
  return axios.post('user/login', loginData, {
@@ -37,27 +81,6 @@ function* logIn(action) {
 
 function* watchLogIn() {
   yield takeEvery(LOG_IN_REQUEST, logIn)
-}
-
-function signUpAPI(signUpData) {
-  return axios.post('/user', signUpData);
-};
-
-function* signUp(action) {
-  try {
-    yield call(signUpAPI, action.data);
-    yield put({
-      type: SIGN_UP_SUCCESS,
-    });
-  } catch (e) {
-    console.error(e);
-    yield put({
-      type: SIGN_UP_FAILURE,
-    })
-  }
-}
-function* watchSignUp() {
-  yield takeLatest(SIGN_UP_REQUEST, signUp)
 }
 
 function logOutAPI() {
@@ -102,12 +125,168 @@ function* loadUser(action) {
     console.error(e);
     yield put({
       type: LOAD_USER_FAILURE,
-      error: e
+      error: e,
     });
   }
 }
 function* watchLoadUser() {
   yield takeEvery(LOAD_USER_REQUEST, loadUser)
+}
+
+function followAPI(userId) {
+  return axios.post(`/user/${userId}/follow`, {}, {
+    withCredentials: true,
+  });
+}
+
+function* follow(action) {
+  try {
+    const result = yield call(followAPI, action.data);
+    yield put({
+      type: FOLLOW_USER_SUCCESS,
+      data: result.data, 
+    });
+  } catch (e) {
+    console.error(e);
+    yield put({
+      type: FOLLOW_USER_FAILURE,
+      error: e,
+    });
+  }
+}
+
+function* watchFollow() {
+  yield takeEvery(FOLLOW_USER_REQUEST, follow);
+}
+
+function unfollowAPI(userId) {
+  return axios.delete(`/user/${userId}/follow`, {
+    withCredentials: true,
+  });
+}
+
+function* unfollow(action) {
+  try {
+    const result = yield call(unfollowAPI, action.data);
+    yield put({
+      type: UNFOLLOW_USER_SUCCESS,
+      data: result.data,
+    });
+  } catch (e) {
+    console.error(e);
+    yield put({
+      type: UNFOLLOW_USER_FAILURE,
+      error: e,
+    });
+  }
+}
+
+function* watchUnfollow() {
+  yield takeEvery(UNFOLLOW_USER_REQUEST, unfollow);
+}
+
+function loadFollowsAPI(userId, offset = 0, limit = 3) {
+  return axios.get(`/user/${userId || 0}/followers?offset=${offset}&limit=${limit}`, {
+    withCredentials: true,
+  });
+}
+
+function* loadFollowers(action) {
+  try {
+    const result = yield call(loadFollowsAPI, action.data, action.offset);
+    yield put({
+      type: LOAD_FOLLOWERS_SUCCESS,
+      data: result.data, 
+    });
+  } catch (e) {
+    console.error(e);
+    yield put({
+      type: LOAD_FOLLOWERS_FAILURE,
+      error: e,
+    });
+  }
+}
+
+function* watchLoadFollowers() {
+  yield takeEvery(LOAD_FOLLOWERS_REQUEST, loadFollowers);
+}
+
+function loadFollowersAPI(userId, offset = 0, limit = 3) {
+  return axios.get(`/user/${userId || 0}/followings?offset=${offset}&limit=${limit}`, {
+    withCredentials: true,
+  });
+}
+
+function* loadFollowings(action) {
+  try {
+    const result = yield call(loadFollowingsAPI, action.data, action.offset);
+    yield put({
+      type: LOAD_FOLLOWINGS_SUCCESS,
+      data: result.data, 
+    });
+  } catch (e) {
+    console.error(e);
+    yield put({
+      type: LOAD_FOLLOWINGS_FAILURE,
+      error: e,
+    });
+  }
+}
+
+function* watchLoadFollowings() {
+  yield takeEvery(LOAD_FOLLOWINGS_REQUEST, loadFollowings);
+}
+
+function removeFollowAPI(userId) {
+  return axios.delete(`/user/${userId}/follower`, {
+    withCredentials: true,
+  });
+}
+
+function* removeFollower(action) {
+  try {
+    const result = yield call(removeFollowerAPI, action.data);
+    yield put({
+      type: REMOVE_FOLLOWER_SUCCESS,
+      data: result.data,
+    });
+  } catch (e) {
+    console.error(e);
+    yield put({
+      type: REMOVE_FOLLOWER_FAILURE,
+      error: e,
+    });
+  }
+}
+
+function* watchRemoveFollower() {
+  yield takeEvery(REMOVE_FOLLOWER_REQUEST, removeFollower);
+}
+
+function editUsernameAPI(username) {
+  return axios.patch('/user/username', { username }, {
+    withCredentials: true,
+  });
+}
+
+function* editUsername(action) {
+  try {
+    const result = yield call(editUsernameAPI, action. data);
+    yield put({
+      type: EDIT_USERNAME_SUCCESS,
+      data: result.data,
+    });
+  } catch (e) {
+    console.error(e);
+    yield put({
+      type: EDIT_USERNAME_FAILURE,
+      error: e,
+    });
+  }
+}
+
+function* watchEditUsername() {
+  yield takeEvery(EDIT_USERNAME_REQUEST, editUsername);
 }
 
 export default function* userSaga() {
@@ -116,5 +295,11 @@ export default function* userSaga() {
     fork(watchLogOut),
     fork(watchLoadUser),
     fork(watchSignUp),
+    fork(watchFollow),
+    fork(watchUnfollow),
+    fork(watchLoadFollowers),
+    fork(watchLoadFollowings),
+    fork(watchRemoveFollower),
+    fork(watchEditUsername),
   ]);
 }
